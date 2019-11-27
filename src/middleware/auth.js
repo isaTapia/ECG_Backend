6a94c306e049c1089cc74c00450db6f56b05450f
isaac141294 <isaac.141294@gmail.com>
2019-11-26 16:43:15 -0800

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/Patient')
-
+const private_key="Word"
 const auth = async(req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const data = jwt.verify(token, "WinterIsComingGOT2019")
+        const data = jwt.verify(token, private_key)
         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
         if (!user) {
             throw new Error()

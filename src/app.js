e14954e94624c220020d1636d18d69188f559cb9
isaac141294 <isaac.141294@gmail.com>
2019-11-27 14:46:03 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ app.set('port', process.env.PORT || 4000);
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(function(req,res,next){
+res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Token");
+res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, TOKEN");
+next();
+});
 // routes
 app.use('/api/patient', require("./routes/patient"));
 app.use('/api/patient/register', require("./routes/patient"));
